8ffeaa697f1de20cc0c44b3270465d565ff0b117
James-Livesey <jamesliveg@gmail.com>
2020-04-19 15:36:48 +0100

    Fix command output capturing

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import re
+
 class GameInterface:
     def __init__(self, cli):
         self.cli = cli
@@ -8,19 +10,17 @@ class GameInterface:
 
         self.cli.writestdin(command + "\n")
         self.cli.writestdin("\n") # Send a blank command so that we know when command output is complete
-        self.cli.poll()
 
         output = ""
         lastMessage = ""
-        timeout = 10
+        timeout = 10000
 
         while timeout != 0:
+            self.cli.poll()
             lastMessage = self.cli.readstdout()
-
-            if lastMessage.startswith("Unknown command: . "): # Test to see if command output is complete
-                break
             
-            output += lastMessage
-            timeout -= 1
-        
-        return output
+            for line in lastMessage.splitlines():
+                if line == "Unknown command: . Please check that the command exists and that you have permission to use it.": # Test to see if command output is complete
+                    return output.rstrip()
+                
+                output += line + "\n"
