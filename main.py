bd13c3d12209116444545f77262d766ab46fe65e
James-Livesey <jamesliveg@gmail.com>
2020-04-20 12:54:34 +0100

    Add plugin config modification detector

--- main.py
+++ main.py
@@ -29,31 +29,43 @@ while not started:
 gameInterfaceInstance.sendChatMessage("§eLoading plugins...")
 print("Loading plugins...")
 
-for plugin in config["plugins"]:
-    plugins.append(importlib.import_module("plugins.{}.main".format(plugin["name"]), plugin["name"]))
-
-    if hasattr(plugins[-1], "__start__"):
-        try:
-            plugins[-1].__start__(game, gameInterfaceInstance, plugin["config"])
-        except Exception as e:
-            gameInterfaceInstance.sendChatMessage("§4Plugin {} failed: uncaught {}".format(plugins[-1].__name__.split(".")[1], e.__class__.__name__))
-            print("Plugin {} failed at start: uncaught {}: {}".format(plugins[-1].__name__.split(".")[1], e.__class__.__name__, e.message))
-    
-    gameInterfaceInstance.sendChatMessage("§ePlugin {} loaded".format(plugins[-1].__name__.split(".")[1]))
-    print("Plugin {} loaded".format(plugins[-1].__name__.split(".")[1]))
-
-if len(plugins) == 1:
-    gameInterfaceInstance.sendChatMessage("§aLoaded 1 plugin")
-    print("Loaded 1 plugin")
-else:
-    gameInterfaceInstance.sendChatMessage("§aLoaded {} plugins".format(len(plugins)))
-    print("Loaded {} plugins".format(len(plugins)))
-
 while True:
-    for plugin in plugins:
-        if hasattr(plugin, "__loop__"):
+    for plugin in config["plugins"]:
+        plugins.append(importlib.import_module("plugins.{}.main".format(plugin["name"]), plugin["name"]))
+
+        if hasattr(plugins[-1], "__start__"):
             try:
-                plugin.__loop__()
+                plugins[-1].__start__(game, gameInterfaceInstance, plugin["config"])
             except Exception as e:
-                gameInterfaceInstance.sendChatMessage("§4Plugin {} failed: uncaught {}".format(plugin.__name__.split(".")[1], e.__class__.__name__))
-                print("Plugin {} failed at loop: uncaught {}: {}".format(plugin.__name__.split(".")[1], e.__class__.__name__, e.message))
+                gameInterfaceInstance.sendChatMessage("§4Plugin {} failed: uncaught {}".format(plugins[-1].__name__.split(".")[1], e.__class__.__name__))
+                print("Plugin {} failed at start: uncaught {}: {}".format(plugins[-1].__name__.split(".")[1], e.__class__.__name__, e.message))
+        
+        gameInterfaceInstance.sendChatMessage("§ePlugin {} loaded".format(plugins[-1].__name__.split(".")[1]))
+        print("Plugin {} loaded".format(plugins[-1].__name__.split(".")[1]))
+
+    if len(plugins) == 1:
+        gameInterfaceInstance.sendChatMessage("§aLoaded 1 plugin")
+        print("Loaded 1 plugin")
+    else:
+        gameInterfaceInstance.sendChatMessage("§aLoaded {} plugins".format(len(plugins)))
+        print("Loaded {} plugins".format(len(plugins)))
+
+    while True:
+        configFile = open("config.json", "r")
+        newConfig = json.loads(configFile.read())
+
+        configFile.close()
+
+        if newConfig != config:
+            break
+
+        for plugin in plugins:
+            if hasattr(plugin, "__loop__"):
+                try:
+                    plugin.__loop__()
+                except Exception as e:
+                    gameInterfaceInstance.sendChatMessage("§4Plugin {} failed: uncaught {}".format(plugin.__name__.split(".")[1], e.__class__.__name__))
+                    print("Plugin {} failed at loop: uncaught {}: {}".format(plugin.__name__.split(".")[1], e.__class__.__name__, e.message))
+    
+    gameInterfaceInstance.sendChatMessage("§ePlugin configuration modified, reloading plugins...")
+    print("Plugin configuration modified, reloading plugins...")
