186f68f7475337c2ec13942862c9b10aa8778f5a
James-Livesey <jamesliveg@gmail.com>
2020-04-20 11:56:54 +0100

    Fix comparison between 2D and 1D array

--- plugins/clock/main.py
+++ plugins/clock/main.py
@@ -198,11 +198,11 @@ def __loop__():
                 dy = 0
                 dz = -4 * i
 
-            if lastTimes[clock][i] != displayedTime[clock][i]:
+            if lastTimes[clock][i] != displayedTime[i]:
                 renderCharacter(clock, game.Point(
                     config["clocks"][clock]["x"] + dx,
                     config["clocks"][clock]["y"] + dy,
                     config["clocks"][clock]["z"] + dz
-                ), displayedTime[clock][i])
+                ), displayedTime[i])
 
         lastTimes[clock] = displayedTime
